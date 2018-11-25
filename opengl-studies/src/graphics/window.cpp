477a3ff430d7ad8ddceb33681f5ea1ab044e7a55
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2018-11-25 21:30:38 -0200

    Fix eof

--- opengl-studies/src/graphics/window.cpp
+++ opengl-studies/src/graphics/window.cpp
@@ -1,5 +1,7 @@
 #include "window.h"
 
+void window_resize(GLFWwindow *window, int width, int height);
+
 Window::Window(const char* title, int width, int height)
 {
 	if (glfwInit())
@@ -12,8 +14,11 @@ Window::Window(const char* title, int width, int height)
 
 		GLFWmonitor* monitor = glfwGetPrimaryMonitor();
 		const GLFWvidmode* mode = glfwGetVideoMode(monitor);
-		glfwSetWindowPos(this->m_Window, (mode->width - width) / 2, (mode->height - height) / 2);
 		glfwMakeContextCurrent(this->m_Window);
+		glfwSetWindowUserPointer(m_Window, this);
+		glfwSetWindowPos(this->m_Window, (mode->width - width) / 2, (mode->height - height) / 2);
+		glfwSetWindowSizeCallback(m_Window, window_resize);
+		glfwSwapInterval(0);
 
 		if (glewInit() != GLEW_OK)
 		{
@@ -44,4 +49,9 @@ void Window::update()
 {
 	glfwSwapBuffers(this->m_Window);
 	glfwPollEvents();
+}
+
+void window_resize(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
--- opengl-studies/src/main.cpp
+++ opengl-studies/src/main.cpp
@@ -9,9 +9,31 @@ int main(void)
 	Window window("OpenGL Studies", WINDOW_WIDTH, WINDOW_HEIGHT);
 	Shader shader("resources/shaders/basic.vsh", "resources/shaders/basic.fsh");
 
+	GLfloat points[] = {
+		 0.0f,  0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f
+	};
+
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
 	while (window.isOpen())
 	{
 		window.clear();
+
+		shader.enable();
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		window.update();
 	}

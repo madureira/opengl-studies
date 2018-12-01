2792dff58cb44e56efa6cde4903a9132712dd320
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2018-12-01 16:13:23 -0200

    Fix eof

--- opengl-studies/src/main.cpp
+++ opengl-studies/src/main.cpp
@@ -15,24 +15,73 @@ int main(void)
 		-0.5f, -0.5f, 0.0f
 	};
 
-	GLuint vbo = 0;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	GLfloat colours[] = {
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 1.0f
+	};
+
+	GLuint pointsVBO = 0;
+	glGenBuffers(1, &pointsVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, pointsVBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 
-	GLuint vao = 0;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	GLuint coloursVBO = 0;
+	glGenBuffers(1, &coloursVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, coloursVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colours), colours, GL_STATIC_DRAW);
+
+	GLuint VAO = 0;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, pointsVBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, coloursVBO);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(1);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CW);
+
+	float matrix[] = {
+		1.0f, 0.0f, 0.0f, 0.0f, // first column
+		0.0f, 1.0f, 0.0f, 0.0f, // second column
+		0.0f, 0.0f, 1.0f, 0.0f, // third column
+		0.5f, 0.0f, 0.0f, 1.0f, // fourth column
+	};
+	
+	int matrixLocation = glGetUniformLocation(shader.getID(), "matrix");
+
+	float speed = 1.0f; // move at 1 unit per second
+	float lastPosition = 0.0f;
 
 	while (window.isOpen())
 	{
-		window.clear();
+		static double previousSeconds = glfwGetTime();
+		double currentSeconds = glfwGetTime();
+		double elapsedSeconds = currentSeconds - previousSeconds;
+		previousSeconds = currentSeconds;
+
+		// reverse direction when going to far left or right
+		if (fabs(lastPosition) > 1.0f)
+		{
+			speed = -speed;
+		}
+
+		// update the matrix
+		matrix[12] = elapsedSeconds * speed + lastPosition;
+		lastPosition = matrix[12];
 
 		shader.enable();
-		glBindVertexArray(vao);
+		glUniformMatrix4fv(matrixLocation, 1, GL_FALSE, matrix);
+		window.clear();
+
+		
+		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		window.update();
